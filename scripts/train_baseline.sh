9096a97de9233d6058ac0a171298a861be0c0a49
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-21 01:29:39 +0800

    cc

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -6,6 +6,6 @@ python -m torch.distributed.launch \
     --master_port=3000 \
     shapeprop/tools/train_net.py \
     --config-file configs/bdd100k_mask_rcnn_r50_fpn_1x.yml \
-    --local_rank 0
+    --local_rank 0 \
     --num_gpu 4
 # Evaluate
