f42f889f602ac1328b4de4310223bb39d932c79a
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-20 22:14:28 +0800

    cc

---
+++ scripts/eval.sh
@@ -0,0 +1,1 @@
+python shapeprop/tools/evaluate.py runs/bdd100k_mask_rcnn_r50_fpn_1x/inference/bdd100k_val/segm.json
--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -7,4 +7,3 @@ python -m torch.distributed.launch \
     shapeprop/tools/train_net.py \
     --config-file configs/bdd100k_mask_rcnn_r50_fpn_1x.yml
 # Evaluate
-python shapeprop/tools/evaluate.py runs/bdd100k_mask_rcnn_r50_fpn_1x/inference/bdd100k_val/segm.json
