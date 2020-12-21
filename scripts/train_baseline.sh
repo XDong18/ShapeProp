7829b9edc7f6563f54e3be81d04f78745544c2da
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-21 10:59:58 +0800

    cc

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -1,11 +1,11 @@
 # Classwise semi-supervision (VOC categories)
 # Train baseline Mask R-CNN
-export CUDA_VISIBLE_DEVICES=1,2,3
+export CUDA_VISIBLE_DEVICES=1
 python -m torch.distributed.launch \
     --nproc_per_node=8 \
     --master_port=3000 \
     shapeprop/tools/train_net.py \
     --config-file configs/bdd100k_mask_rcnn_r50_fpn_1x.yml \
     --local_rank 0 \
-    --num_gpu 3
+    --num_gpu 1
 # Evaluate
