326a19df58a77e02024d8accfa3f7e75808c0fc8
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-21 18:03:32 +0800

    cc

--- shapeprop/data/build.py
+++ shapeprop/data/build.py
@@ -158,7 +158,7 @@ def make_data_loader(cfg, is_train=True, is_distributed=False, start_iter=0):
 
     data_loaders = []
     for dataset in datasets:
-        sampler = make_data_sampler(dataset, shuffle, is_distributed)
+        sampler = make_data_sampler(dataset, shuffle, False) #TODO change distributed to False
         batch_sampler = make_batch_data_sampler(
             dataset, sampler, aspect_grouping, images_per_gpu, num_iters, start_iter
         )
