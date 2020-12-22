7c481266aba1f53b7ffa95579df4a139d2e1c103
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-22 19:32:54 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=1
 python shapeprop/tools/train_net.py \
-    --config-file configs/bdd100k_mask_rcnn_r50_fpn_1x.yml \
+    --config-file configs/bdd100k_mask_rcnn_r50_fpn_3x.yml \
     --local_rank 1 \
     --num_gpu 1
