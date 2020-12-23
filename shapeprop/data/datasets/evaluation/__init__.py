e2e6c84d10e3d6ddcf684d2322fde7b17f224f24
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-23 15:08:27 +0800

    cc

--- shapeprop/data/datasets/evaluation/__init__.py
+++ shapeprop/data/datasets/evaluation/__init__.py
@@ -17,8 +17,8 @@ def evaluate(dataset, predictions, output_folder, **kwargs):
     args = dict(
         dataset=dataset, predictions=predictions, output_folder=output_folder, **kwargs
     )
-    if isinstance(dataset, datasets.COCODataset):
-        return coco_evaluation(**args)
-    else:
-        dataset_name = dataset.__class__.__name__
-        raise NotImplementedError("Unsupported dataset type {}.".format(dataset_name))
+    # if isinstance(dataset, datasets.COCODataset):
+    return coco_evaluation(**args)
+    # else:
+    #     dataset_name = dataset.__class__.__name__
+    #     raise NotImplementedError("Unsupported dataset type {}.".format(dataset_name))
