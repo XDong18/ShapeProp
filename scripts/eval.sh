ec5335f1f1d23e18847d31fafaf83a65dff0c100
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-29 15:39:56 +0800

    solov2

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,1 +1,1 @@
-python shapeprop/tools/evaluate.py runs/bdd100k_mask_rcnn_r50_fpn_1x/inference/bdd100k_val/segm.json
+python shapeprop/tools/evaluate.py runs/bdd100k_mask_rcnn_r50_fpn_yanzhao/inference/bdd100k_val/segm.json
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=9
+export CUDA_VISIBLE_DEVICES=5
 python shapeprop/tools/train_net.py \
     --config-file configs/bdd_yanzhao_r50.yml \
     --local_rank 1 \
